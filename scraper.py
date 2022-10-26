e170ffcfbb95a58645ad5fc889407a244a2d0af0
AlexanderHiliou <gilevsasha17@gmail.com>
2022-10-27 02:24:41 +0300

    add zeelproj scraper

--- scraper.py
+++ scraper.py
@@ -5,7 +5,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-accounts = {'botscrap1@mail.ru': 'vB7nPN9185', 'botscrap2@mail.ru': 'vB7nPN9185', 'botscrap3@mail.ru': 'vB7nPN9185', 'botscrap4@mail.ru': 'vB7nPN9185', 'botscrap5@mail.ru': 'vB7nPN9185'}
+import ast
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+accounts = os.getenv("ACCOUNTS")
+
+# transform str to a dict
+accounts = ast.literal_eval(accounts)
+
 
 class Scraper:
 
@@ -17,14 +27,16 @@ class Scraper:
             EC.presence_of_element_located((By.XPATH, f'{xpath}'))
         )
         if value_to_insert:
-            self.driver.find_element(By.XPATH, f'{xpath}').send_keys(value_to_insert)
+            self.driver.find_element(
+                By.XPATH, f'{xpath}').send_keys(value_to_insert)
         self.driver.find_element(By.XPATH, f'{xpath}').click()
+
 
 class ZeelProject(Scraper):
 
     _zeelproject = 'https://zeelproject.com/ru/'
 
-    def __init__(self, link:str, email:str, password:str):
+    def __init__(self, link: str, email: str, password: str):
         super().__init__()
         self.link = link
         self.email = email
@@ -39,21 +51,23 @@ class ZeelProject(Scraper):
         self.wait_and_find_element('//*[@id="login_password"]')
         self.wait_and_find_element('//*[@id="login_password"]', self.password)
         self.wait_and_find_element('//*[@id="submit_password"]')
-    
+
     def download(self):
         self.driver.get(self.link)
-        download_counter = self.driver.find_element(By.CLASS_NAME, 'downloaded')
+        download_counter = self.driver.find_element(
+            By.CLASS_NAME, 'downloaded')
         if download_counter.text == '0':
-            self.driver.find_element(By.PARTIAL_LINK_TEXT, 'Скачать 3D модель').click()
+            self.driver.find_element(
+                By.PARTIAL_LINK_TEXT, 'Скачать 3D модель').click()
             return True
         self.driver.close()
         return False
 
 
-   
 if __name__ == '__main__':
     for k, v in accounts.items():
-        proj = ZeelProject('https://zeelproject.com/ru/32623-coffee-table-heritage.html', k, v)
+        proj = ZeelProject(
+            'https://zeelproject.com/ru/32623-coffee-table-heritage.html', k, v)
         proj.login()
         if proj.download():
             break
